ba458b6e28ea64edceb30d360d8b3d88353644c3
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-05-03 12:21:01 +0500

    added profile route and created structure for profile component

--- Frontend/Project/src/app/profile/profile.component.ts
+++ Frontend/Project/src/app/profile/profile.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-profile',
+  standalone: true,
   imports: [],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
