a16a29e7f8aa36048850b565f48c892d5527ae37
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-05-04 01:55:32 +0500

    created simple jwt to allow authorization

--- Backend/environment/Scripts/Project/Project/settings.py
+++ Backend/environment/Scripts/Project/Project/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+
+
 ROOT_URLCONF = 'Project.urls'
 
 TEMPLATES = [
@@ -100,6 +103,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
--- Backend/environment/Scripts/Project/api/urls.py
+++ Backend/environment/Scripts/Project/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('posts/', PostListView.as_view(), name='post-list'),
     path('posts/create/', CreatePostView.as_view(), name='post-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
