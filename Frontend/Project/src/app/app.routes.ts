59a906f050155b9210b06f3d64c942fd5f1c7e5e
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-05-04 06:21:54 +0500

    created post detail component

--- Frontend/Project/src/app/app.routes.ts
+++ Frontend/Project/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthComponent } from './auth/auth.component';
 import { CreatePostComponent } from './create-post/create-post.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: AuthComponent},
     {path: 'profile', component: ProfileComponent},
-    {path: 'create-post', component: CreatePostComponent}
+    {path: 'create-post', component: CreatePostComponent},
+    {path: 'posts/:id', component: PostDetailComponent}
 ];
